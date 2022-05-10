badb4812441bed1ab18d5ebd53cd418c99808e1b
smeea-2018 <smeeaarshad@gmail.com>
2022-05-10 11:38:38 +0100

    function to render questions

--- assets/js/index.js
+++ assets/js/index.js
@@ -42,6 +42,37 @@ const main = document.getElementById("main");
 const timerSpan = document.getElementById("timer-span");
 const formElement = document.getElementById("submit-score-button");
 
+//Function to render questions
+const renderQuestion = () => {
+  //Render the first question
+
+  const currentQuestion = questions[questionIndex];
+
+  const section = document.createElement("section");
+  // section.setAttribute("class", "form-section");
+  section.setAttribute("class", "question-section");
+  section.setAttribute("id", "question-container");
+  //section.setAttribute("data-correct-answer", "Both var and let");
+
+  const h2 = document.createElement("h2");
+  h2.setAttribute("class", "subtitle");
+  h2.textContent = currentQuestion.text;
+
+  const ul = document.createElement("ul");
+  ul.setAttribute("class", "unordered-list");
+  for (let i = 0; i < currentQuestion.options.length; i += 1) {
+    const li = document.createElement("li");
+    li.setAttribute("class", "list-item");
+    li.setAttribute("data-value", currentQuestion.options[i]);
+    li.textContent = currentQuestion.options[i];
+    ul.appendChild(li);
+  } //end for
+
+  section.append(h2, ul);
+  main.append(section);
+  section.addEventListener("click", selectAnswer);
+};
+
 const startQuiz = () => {
   //Remove start quiz section
   const sectionStart = document.getElementById("start-quiz-id");
